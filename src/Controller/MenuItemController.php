db3329f9cd65e67ab7051683f9ec17da3cbf3a2c
Luca Chaboissier <lucachaboissier@outlook.com>
2024-12-17 11:22:05 +0100

    link issue

--- src/Controller/MenuItemController.php
+++ src/Controller/MenuItemController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\MenuItem;
 use App\Form\MenuItemType;
+use App\Repository\MenuItemRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
